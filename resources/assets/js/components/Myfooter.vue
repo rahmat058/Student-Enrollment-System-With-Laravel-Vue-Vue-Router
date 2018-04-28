160311972dd4051e1c3ee35a5d7adf19a3d9dcac
Kazi Rahamatullah <kazirahmat58@gmail.com>
2018-04-28 13:28:07 +0600

    Header and Footer is added

---
+++ resources/assets/js/components/Myfooter.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer class="footer">
+ <div class="container">
+   <div class="content has-text-centered">
+     <p>
+       <strong>Student Enrollment App With Vue+Laravel</strong> by <a href="https://github.com/rahmat058">Kazi Rahamatullah</a>. Access The Source Code
+       <a href="https://github.com/rahmat058/Student-Enrollment-System-With-Laravel-Vue-Vue-Router">Github</a> 
+     </p>
+   </div>
+ </div>
+</footer>
+</template>
+
+<script>
+export default {
+}
+</script>
---
+++ resources/assets/js/components/Myheader.vue
@@ -0,0 +1,32 @@
+<template>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+ <div class="navbar-brand">
+   <a class="navbar-item" href="https://bulma.io">
+     <img src="https://bulma.io/images/bulma-logo.png" alt="" width="112" height="28">
+   </a>
+
+   <div class="navbar-menu is-active">
+      <a class="navbar-item" href="">
+         Home
+      </a>
+
+      <a class="navbar-item" href="">
+         Add Student
+      </a>
+   </div>
+
+
+   <div class="navbar-burger">
+     <span></span>
+     <span></span>
+     <span></span>
+   </div>
+ </div>
+</nav>
+</template>
+
+
+<script>
+export default {
+}
+</script>
